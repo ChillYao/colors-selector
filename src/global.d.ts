237fb8505742aa66c17e4cfbaf00f784bba383a6
tong.yao <tong.yao@check24.de>
2023-08-02 15:02:32 +0200

    autocomplete and typing the global

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,13 @@
+import { AdjustColorActions } from './components/color-reducer';
 
+type PropsWithHexColor<P> = P & {
+  hexColor: string;
+};
+
+type PorpsWithDispatch<P> = P & {
+  dispatch: React.Dispatch<AdjustColorActions>;
+};
+
+type PropsWithHexColorAndDispatch<P> = P &
+  PropsWithHexColor &
+  PorpsWithDispatch;
